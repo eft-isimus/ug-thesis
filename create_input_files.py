c570e4ed52110710d4b4d4791c0291dd879768e3
eft-isimus <ayushgurule9@gmail.com>
2026-01-28 16:25:01 +0530

    Fixed a bug where the list was being multiplied with floats.
    line: box = .... (was N_m*..., changed to np.max(N_m)*...)

--- create_input_files.py
+++ create_input_files.py
@@ -17,22 +17,35 @@ inputs = json.loads(sys.argv[1])
 path   = sys.argv[2]
 
 # Example input:
-# inputs = system_inputs   = {"ad_strength":0, "ad_cutoff": 2.5, "m": 10000, "polymer_seperation": 6, "N_p": 400, "N_m": 20, "bond_length": 1.12246, "k": 0, "T":0.1, "dt":0.001, "t_f":10000, "m":10000, "num_runs":1}
+# inputs = system_inputs   = {"ad_strength":0, "ad_cutoff": 2.5, "m": 10000, "polymer_seperation": 6, "N_p": [12, 4], "N_m": [20, 40], "bond_length": 1.12246, "k": 0, "T":0.1, "dt":0.001, "t_f":100, "m":10000, "num_runs":1, "mixed":1}
 
 def create_input_file(run_index, **kwargs):
     ad_strength        = kwargs.get('ad_strength', 1)          # NOTE: NOT NEEDED, only there due to laziness
     ad_cutoff          = kwargs.get('ad_cutoff', 2.5)          # NOTE: NOT NEEDED, only there due to laziness
     sigma              = kwargs.get('sigma', 1)                # size of monomers
     polymer_seperation = kwargs.get('polymer_seperation', 10)  # distance between a polymer and its 4 nearest neighbors
-    N_p                = kwargs.get('N_p', 2)                  # total number of polymers
-    N_m                = kwargs.get('N_m', 10)                 # number of monomers in each polymer
+    N_p                = kwargs.get('N_p', 4)                  # list of number of polymers of each type [small to large]
+    N_m                = np.array(kwargs.get('N_m', [10]))     # list of number of monomers in each polymer [small to large]
     bond_length        = kwargs.get('bond_length', 1)          # SET TO 1 ALWAYS
-    box_len            = (np.sqrt(N_p)) * polymer_seperation   # calculating box length to make box
     m                  = kwargs.get('m', 10_000)
+    mixed              = kwargs.get('mixed', False)
+    
+    # 1 = mixed, 0 = not mixed
+    if mixed == 1:
+        mixed = True
+    else:
+        mixed = False
+    total_N_p          = np.sum(N_p)
+    box_len            = (np.sqrt(total_N_p)) * polymer_seperation   # calculating box length to make box
+    
+    if not mixed:
+        n_m = N_m[0] # for convenience when the brush is not mixed
+
+
     # the box size needs to be such that the edge and corner polymers also see the same brush
-    box = [[-box_len/2, box_len/2], [-box_len/2, box_len/2], [-5, 2*N_m*bond_length]]  # box size
+    box = [[-box_len/2, box_len/2], [-box_len/2, box_len/2], [-5, 1.2*np.max(N_m)*bond_length]]  # box size
     k   = kwargs.get('k', 0)                                                                     # stiffness of polymers
-    rho = N_p/(box_len**2)                                                         # density of polymer brush
+    rho = total_N_p/(box_len**2)   # density of polymer brush
 
     # first set of commands define the units, styles, sim box, computes etc.
     commands1 = f"""
@@ -41,7 +54,7 @@ def create_input_file(run_index, **kwargs):
 #---------------
 units lj
 atom_style molecular
-boundary p p f    
+boundary p p f
 dimension 3
 
 #---------------
@@ -53,13 +66,33 @@ angle_style cosine/squared
 # WCA potential (LJ 12-6 with diff. cutoff and shift)
 pair_style lj/cut {1.12246*sigma}                      # cutoff = 2^(1/6) sigma
 pair_modify shift yes                                       # shift the potential to remove discontinuity
+"""
+    if mixed:
+        commands1 += f"""
+read_data ../{N_m[0]}_{N_m[1]}_Nm_{N_p[0]}_{N_p[1]}_Np_{np.round(rho, decimals=4)}rho.data"""
+    elif not mixed:
+        commands1 += f"""
+read_data ../{N_m[0]}_Nm_{N_p[0]}_Np_{np.round(rho, decimals=4)}rho.data"""
 
-read_data ../{N_m}Nm_{N_p}Np_{np.round(rho, decimals=4)}rho.data
-
+    if mixed:
+        commands1 += """
 mass 1 1.0
+mass 2 1.0
+mass 3 1.0
+mass 4 1.0
+
+"""
+    if not mixed:
+        commands1 += """
+mass 1 1.0
+mass 2 1.0
+
+"""
+    commands1 += f"""
 bond_coeff 1 30.0 1.5 1.0 1.0
 angle_coeff 1 {k} 180.0
-pair_coeff 1 1 1.0 {sigma} {1.12246*sigma}        # set cutoff as 2^(1/6) sigma again
+pair_coeff * * 1.0 {sigma} {1.12246*sigma}        # set cutoff as 2^(1/6) sigma again
+
 
 special_bonds fene
 
@@ -79,14 +112,27 @@ special_bonds fene
     # ----------
 
     data_file = f"""
-{(N_m) * N_p} atoms 
-{(N_m - 1) * N_p} bonds
-{(N_m - 2) * N_p} angles
-
-1 atom types
+{np.sum(N_m * N_p)} atoms 
+{np.sum((N_m - 1) * N_p)} bonds
+{np.sum((N_m - 2) * N_p)} angles
+"""
+# need 2 atom types, one for base atoms and one for non-base atoms
+    if not mixed:
+         data_file += f""" 
+2 atom types
 1 bond types
 1 angle types
+"""
 
+# need 4 atom types for mixed brushes, two for base atoms and two for non-base atoms
+    elif mixed:
+         data_file += f""" 
+4 atom types
+1 bond types
+1 angle types
+"""
+
+    data_file += f"""
 {box[0][0]} {box[0][1]} xlo xhi
 {box[1][0]} {box[1][1]} ylo yhi
 {box[2][0]} {box[2][1]} zlo zhi
@@ -94,56 +140,127 @@ special_bonds fene
 Atoms
 
 """
+    if mixed:
+        choice_array = [0]*N_p[0] + [1]*N_p[1]
+        np.random.shuffle(choice_array)
+    
     mol_id = 1
-    for xi in range(int(np.sqrt(N_p))):
-        for yi in range(int(np.sqrt(N_p))):
-            x_coord = (xi * polymer_seperation) - ((int(np.sqrt(N_p)) - 1) * polymer_seperation)/2 # centers at 0
-            y_coord = (yi * polymer_seperation) - ((int(np.sqrt(N_p)) - 1) * polymer_seperation)/2 # centers at 0
-#            mol_id = (xi * N_m + yi) + 1 # to ensure mol_id starts from 1 not 0
+    atom_id = 1
+    for xi in range(int(np.sqrt(total_N_p))):
+        for yi in range(int(np.sqrt(total_N_p))):
+            # x-y coords remain same regardless of mixed brushes
+            x_coord = (xi * polymer_seperation) - ((int(np.sqrt(total_N_p)) - 1) * polymer_seperation)/2 # centers at 0
+            y_coord = (yi * polymer_seperation) - ((int(np.sqrt(total_N_p)) - 1) * polymer_seperation)/2 # centers at 0
+            if not mixed:
+                for atm in range(n_m):
+                    z_coord = atm * bond_length
+                    # atom-id mol-id type x y z
+                    # base atoms = type 2, non-base = type 1
+                    if atm == 0:
+                        data_file += f"""{atom_id} {mol_id} 2 {x_coord:.6f} {y_coord:.6f} {z_coord:.6f}\n"""
+                    else:
+                        data_file += f"""{atom_id} {mol_id} 1 {x_coord:.6f} {y_coord:.6f} {z_coord:.6f}\n"""
+                    atom_id += 1
+                mol_id += 1
+            
+            # pol-1 = 1, base-atom-1 = 2, pol-2 = 3, base-2 = 4
+            if mixed:
+                num_monomers = N_m[choice_array[mol_id - 1]]
+                if num_monomers == N_m[0]:
+                    for atm in range(num_monomers):
+                        z_coord = atm * bond_length
+                        # atom-id mol-id type x y z
+                        if atm == 0:
+                            data_file += f"""{atom_id} {mol_id} 2 {x_coord:.6f} {y_coord:.6f} {z_coord:.6f}\n"""
+                        else:
+                            data_file += f"""{atom_id} {mol_id} 1 {x_coord:.6f} {y_coord:.6f} {z_coord:.6f}\n"""
+                        atom_id += 1
+                elif num_monomers == N_m[1]:
+                    for atm in range(num_monomers):
+                        z_coord = atm * bond_length
+                        # atom-id mol-id type x y z
+                        if atm == 0:
+                            data_file += f"""{atom_id} {mol_id} 4 {x_coord:.6f} {y_coord:.6f} {z_coord:.6f}\n"""
+                        else:
+                            data_file += f"""{atom_id} {mol_id} 3 {x_coord:.6f} {y_coord:.6f} {z_coord:.6f}\n"""
+                        atom_id += 1
+                mol_id += 1
 
-            for atm in range(N_m):
-                z_coord = atm * bond_length
-                # atom-id mol-id type x y z
-                data_file += f"""{(mol_id - 1) * N_m + atm + 1} {mol_id} 1 {x_coord:.6f} {y_coord:.6f} {z_coord:.6f}\n"""
-            mol_id += 1
-    bond_count = N_p * (N_m - 1)
+    bond_count = {np.sum((N_m - 1) * N_p)}
     data_file += f"""\nBonds\n\n"""
 
+    mol_id = 1 
     bond_id = 1
-    for pol in range(N_p):
-        first_atom = pol * N_m
-        for bond in range(N_m - 1):
-            a1 = first_atom + bond + 1
-            a2 = first_atom + bond + 2
-            data_file += f"""{bond_id} 1 {a1} {a2}\n"""
-            bond_id += 1
+    first_atom = 1
+    for pol in range(total_N_p):
+        if not mixed:
+            first_atom = pol * n_m
+            for bond in range(n_m - 1):
+                a1 = first_atom + bond + 1
+                a2 = first_atom + bond + 2
+                data_file += f"""{bond_id} 1 {a1} {a2}\n"""
+                bond_id += 1
+            mol_id += 1
+        if mixed:
+            num_monomers = N_m[choice_array[mol_id - 1]]
+            for bond in range(num_monomers - 1):
+                a1 = first_atom + bond 
+                a2 = first_atom + bond + 1
+                data_file += f"""{bond_id} 1 {a1} {a2}\n"""
+                bond_id += 1
+            mol_id += 1
+            first_atom += num_monomers
 
     data_file += f"""\nAngles\n\n"""
+    mol_id = 1
     angle_id = 1
-    for pol in range(N_p):
-        first_atom = pol * N_m
-        for angle in range(N_m - 2):
-            a1 = first_atom + angle + 1
-            a2 = first_atom + angle + 2
-            a3 = first_atom + angle + 3
-            data_file += f"""{angle_id} 1 {a1} {a2} {a3}\n"""
-            angle_id += 1
-    
-    with open(path + f"/{N_m}Nm_{N_p}Np_{np.round(rho, decimals=4)}rho.data", "w") as f:
-        f.write(data_file)
-    
-    commands2 = f"""         
-    # creating adsorption groups, plane and walls
-    group base_atoms id 1:{N_p*N_m}:{N_m}                        # fixing all of the base atoms
-    group mobile_atoms subtract all base_atoms # creating group of atoms which can move
+    first_atom = 1
+    for pol in range(total_N_p):
+        if not mixed:
+            first_atom = pol * n_m
+            for angle in range(n_m - 2):
+                a1 = first_atom + angle + 1
+                a2 = first_atom + angle + 2
+                a3 = first_atom + angle + 3
+                data_file += f"""{angle_id} 1 {a1} {a2} {a3}\n"""
+                angle_id += 1
+            mol_id += 1
+        if mixed:
+            num_monomers = N_m[choice_array[mol_id - 1]]
+            for angle in range(num_monomers - 2):
+                a1 = first_atom + angle 
+                a2 = first_atom + angle + 1
+                a3 = first_atom + angle + 2
+                data_file += f"""{angle_id} 1 {a1} {a2} {a3}\n"""
+                angle_id += 1
+            mol_id += 1
+            first_atom += num_monomers
+    if mixed:
+        with open(path + f"/{N_m[0]}_{N_m[1]}_Nm_{N_p[0]}_{N_p[1]}_Np_{np.round(rho, decimals=4)}rho.data", "w") as f:
+            f.write(data_file)
+    elif not mixed:
+        with open(path + f"/{N_m[0]}_Nm_{N_p[0]}_Np_{np.round(rho, decimals=4)}rho.data", "w") as f:
+            f.write(data_file)
+    if mixed:
+        commands2 = f"""         
+# creating adsorption groups, plane and walls
+group base_atoms type 2 4                        # fixing all of the base atoms 
+"""
+    elif not mixed:
+        commands2 = f"""         
+# creating adsorption groups, plane and walls
+group base_atoms type 2                        # fixing all of the base atoms 
+"""     
+    commands2 += """
+group mobile_atoms subtract all base_atoms # creating group of atoms which can move
 
-    # no adsorption plane for now                                                                
-    # fix wall all wall/lj93 zlo 0.0 {ad_strength} 1.0 {ad_cutoff}    # adsorption wall
-    # fix_modify wall energy yes
+# no adsorption plane for now                                                                
+# fix wall all wall/lj93 zlo 0.0 {ad_strength} 1.0 {ad_cutoff}    # adsorption wall
+# fix_modify wall energy yes
 
-    # fix zwall all wall/reflect zlo 0 zhi {2*N_m*bond_length}                                  # impenetrable wall
-    fix zwall all wall/reflect zlo 0 zhi {2*N_m*1.5}                                  # 1.5 is the max length of a bond in FENE
-    
+# fix zwall all wall/reflect zlo 0 zhi {2*N_m*bond_length}                                  # impenetrable wall
+fix zwall all wall/reflect zlo 0 zhi {1.2*np.max(N_m)*1.5}                                  # 1.5 is the max length of a bond in FENE
+
     """
 
     T = kwargs.get('T', 0)
@@ -151,40 +268,53 @@ Atoms
     t_f = kwargs.get('t_f', 0.0001)
     m = kwargs.get('m', 1000)
 
-    commands3 = f"""
-    # for post-processing in python
-    dump 1 all custom {m} {N_m}Nm_{N_p}Np_{np.round(rho, decimals=4)}rho_{T}T_{t_f}tf_{run_index}ri.poly id mol type x y z    
-    # for dumping atom coords in a sensible order (id 1, 2, 3, ...)
-    dump_modify 1 sort id                           
-    velocity all create {T} {np.random.randint(100_000, 999_999)} mom yes rot yes          # random seed = statistically ind. runs
-    neighbor 1.0 bin
-    neigh_modify delay 100 every 5 check yes                                                      # hard requirement for minimize
-    comm_modify cutoff 3.0
-    timestep {dt}
-
-    fix 1 all langevin {T} {T} {100*dt} {np.random.randint(100_000, 999_999)}    # random seed = statistically ind. runs
-    fix 2 all nve
-
-    # fix just the base atoms to always be attached
-    fix freeze_force base_atoms setforce 0.0 0.0 0.0
-    fix_modify freeze_force energy no
-    velocity base_atoms set 0.0 0.0 0.0
-    compute blen all bond/local dist
-    compute avg_blen all reduce ave c_blen
-
-    fix blen_out all ave/time 50000 1 50000 c_avg_blen file avg_bond_length.dat
-
-    restart {int(t_f/(dt*10))} {N_m}Nm_{N_p}Np_{np.round(rho, decimals=4)}rho_{T}T_{t_f}tf_{run_index}ri_restart.bin # creates restart files every tenth of the way
-    run {int(t_f/dt)}
+    if mixed:
+        commands3 = f"""
+# for post-processing in python
+dump 1 all custom {m} {N_m[0]}_{N_m[1]}_Nm_{N_p[0]}_{N_p[1]}_Np_{np.round(rho, decimals=4)}rho_{T}T_{t_f}tf_{run_index}ri.poly id mol type x y z    
     """
+    elif not mixed:
+        commands3 = f""" 
+# for post-processing in python
+dump 1 all custom {m} {N_m[0]}_Nm_{N_p[0]}_Np_{np.round(rho, decimals=4)}rho_{T}T_{t_f}tf_{run_index}ri.poly id mol type x y z    
+"""
+    commands3 += f"""
+# for dumping atom coords in a sensible order (id 1, 2, 3, ...)
+dump_modify 1 sort id                           
+velocity all create {T} {np.random.randint(100_000, 999_999)} mom yes rot yes          # random seed = statistically ind. runs
+neighbor 1.0 bin
+neigh_modify delay 100 every 5 check yes                                                      # hard requirement for minimize
+comm_modify cutoff 3.0
+timestep {dt}
 
+fix 1 all langevin {T} {T} {100*dt} {np.random.randint(100_000, 999_999)}    # random seed = statistically ind. runs
+fix 2 all nve
+
+# fix just the base atoms to always be attached
+fix freeze_force base_atoms setforce 0.0 0.0 0.0
+fix_modify freeze_force energy no
+velocity base_atoms set 0.0 0.0 0.0"""
+
+    if mixed:
+        commands3 += f"""
+restart {int(t_f/(dt*10))} {N_m[0]}_{N_m[1]}_Nm_{N_p[0]}_{N_p[1]}_Np_{np.round(rho, decimals=4)}rho_{T}T_{t_f}tf_{run_index}ri_restart.bin # creates restart files every tenth of the way
+run {int(t_f/dt)}
+    """
+    elif not mixed:
+        commands3 += f"""
+restart {int(t_f/(dt*10))} {N_m[0]}_Nm_{N_p[0]}_Np_{np.round(rho, decimals=4)}rho_{T}T_{t_f}tf_{run_index}ri_restart.bin # creates restart files every tenth of the way
+run {int(t_f/dt)}
+    """
+        
     string = commands1 + commands2 + commands3
     
-    with open(path + f"/{N_m}Nm_{N_p}Np_{np.round(rho, decimals=4)}rho_{T}T_{t_f}tf_{run_index}ri_input.sim", "w") as f:
-        f.write(string)
-    
+    if mixed:
+        with open(path + f"/{N_m[0]}_{N_m[1]}_Nm_{N_p[0]}_{N_p[1]}_Np_{np.round(rho, decimals=4)}rho_{T}T_{t_f}tf_{run_index}ri_input.sim", "w") as f:
+            f.write(string)
+    elif not mixed:
+        with open(path + f"/{N_m[0]}_Nm_{N_p[0]}_Np_{np.round(rho, decimals=4)}rho_{T}T_{t_f}tf_{run_index}ri_input.sim", "w") as f:
+            f.write(string)
     # return string
 
 for i in range(int(inputs['num_runs'])):
     create_input_file(i + 1, **inputs)
-
